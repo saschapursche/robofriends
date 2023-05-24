fc3a8c60ca04bcaf01c963def331b0341695f773
saschapursche <sascha_pursche@outlook.de>
2023-05-24 22:42:15 +0200

    added first component

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Card = () => {
+    return (
+        <div>
+            <img alt= "robot_photo" src= "https://robohash.org/wurstbaum?200x200"/>
+            <div>
+                <h2>Jane Doe</h2>
+                <p>jane.doe@gmail.com</p>
+            </div>
+        </div>
+    )
+}
+
+export default Card;
